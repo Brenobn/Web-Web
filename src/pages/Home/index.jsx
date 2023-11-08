d3b7658ddcad72ca1cfac90809a718aaed6310e7
Brenobn <brenobragabn@gmail.com>
2023-11-08 10:21:50 -0300

    Adding more dependencies

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,31 @@
-import { Link } from "react-router-dom";
-
+import bannerImg from '../../assets/banner.png';
 import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+
+import { Container, TextContent, Title, TitleHighlight } from './styles';
 
 const Home = () => {
   return(
     <>
-      <h1>Home</h1>
-      <Button title="Teste 1" />
-      <Button variant="secondary" title="Teste 2" />
-      <Link to="/login" >Fazer login</Link>
+     <Header />
+     <Container>
+      <div>
+        <Title>
+          <TitleHighlight>
+            Implemente
+            <br />
+          </TitleHighlight>
+          o seu futuro global agora!
+        </Title>
+        <TextContent>
+          Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+        </TextContent>
+        <Button title="Começar agora" variant="secondary" onClick={() => null} /> 
+      </div>
+      <div>
+        <img src={bannerImg} alt="Imagem principal" />
+      </div>
+     </Container>
     </>
   );
 }
