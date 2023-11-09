53932fc8755c320a53f53acdc298e803c00f5339
Brenobn <brenobragabn@gmail.com>
2023-11-09 10:14:18 -0300

    Crated new page login with html structure and style

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,46 @@
-import { Link } from "react-router-dom";
+import { MdEmail, MdLock } from 'react-icons/md';
+
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from '../../components/Input';
+
+import { Container, CreateText, ForgotPassword, Row, SubtitleLogin, Title, TitleLogin, Wrapper, Column } from './styles';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const handleClickSignIn = () => {
+    navigate('/feed');
+  }
+
   return(
     <>
-      <h1>Login</h1>
-      <Link to="/" >Voltar para home</Link>
+     <Header />
+     <Container>
+      <Column>
+        <Title>
+          A plataforma para você aprender com experts, dominar as pricipais tecnologias e entrar mais rapido nas empresas mais desejadas.
+        </Title> 
+      </Column>
+      <Column>
+        <Wrapper>
+          <TitleLogin>Faça seu cadastro</TitleLogin>
+          <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+          <form>
+            <Input placeholder="E-mail" leftIcon={<MdEmail />} />
+            <Input placeholder="Senha" type="password" leftIcon={<MdLock />} />
+            <Button title="Entrar" variant="secondary" onClick={handleClickSignIn} type="button" />
+          </form>
+          <Row>
+            <ForgotPassword>Esqueci minha senha</ForgotPassword>
+            <CreateText>Criar Conta</CreateText>
+          </Row>
+        </Wrapper>
+      </Column>
+     </Container>
     </>
   );
 }
